ee47adfdd1d9cb8e2d1bbebe4117ea5a8d0e5efa
SimonDanisch <sdanisch@gmail.com>
2017-11-24 14:54:15 +0100

    copy & paste script

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,6 +1,4 @@
 using BinaryBuilder
-
-
 platforms = [
   BinaryProvider.Linux(:i686, :glibc),
   BinaryProvider.Linux(:x86_64, :glibc),
@@ -11,12 +9,10 @@ platforms = [
   BinaryProvider.Windows(:i686),
   BinaryProvider.Windows(:x86_64)
 ]
-
 sources = [
-    "https://github.com/JuliaGeometry/EarCut.jl.git" =>
-    "4db9500d402151d19b831983a6de94fdeb9435ce",
+  "https://github.com/JuliaGeometry/EarCut.jl.git" =>
+"4db9500d402151d19b831983a6de94fdeb9435ce",
 ]
-
 script = raw"""
 cd $WORKSPACE/srcdir
 cd EarCut.jl/deps/
@@ -24,16 +20,13 @@ g++ -c -fPIC -std=c++11 cwrapper.cpp -o earcut.o
 g++ -shared -o earcut.so earcut.o
 mv earcut.so $DESTDIR
 exit
-
 if [ $target = "x86_64-w64-mingw32" ]; then
 cd $WORKSPACE/srcdir
 g++ -c -fPIC -std=c++11 cwrapper.cpp -o earcut.o
 g++ -shared -o earcut.dll earcut.o
 mv earcut.dll $DESTDIR
 exit
-
 fi
-
 if [ $target = "x86_64-w64-mingw32" ]; then
 cd $WORKSPACE/srcdir
 ls
@@ -45,12 +38,9 @@ g++ -c -fPIC -std=c++11 cwrapper.cpp -o earcut.o
 g++ -shared -o earcut.dll earcut.o
 mv earcut.dll  $DESTDIR
 exit
-
 fi
 """
-
 products = prefix -> [
-	LibraryProduct(prefix, "earcut")
+	LibraryProduct(prefix,"earcut")
 ]
-
 autobuild(pwd(), "EarCut", platforms, sources, script, products)
